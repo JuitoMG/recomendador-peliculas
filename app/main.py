9dcb03f587f6c33c8e519cc205ea5cfe4afc40a5
JuitoMG <juitomg@gmail.com>
2025-06-14 18:45:51 +0200

    Terminado un index básico que aparece con FastAPI arrancado

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Recomendador de Películas por IA")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+templates = Jinja2Templates(directory="app/templates")
+
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return "<h1>Recomendador de películas en marcha!</h1>"
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/recomendar", response_class=HTMLResponse)
+async def recomendar(request: Request, pelicula_base: str = Form(...)):
+    return HTMLResponse(f"<h2>Película recibida: {pelicula_base}</h2>")
